6b39afce249f15b3b3b42e11e71be6e79d911d51
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-23 12:23:44 +0900

    Add postcss.config.js, src/main.js, jsconfig.json, .vscode/extensions.json, .prettierrc.json, src/App.vue, .eslintrc.cjs, src/assets/main.css, .gitignore, vite.config.js, index.html, tailwind.config.js, README.md, package.json, src/components/WelcomeItem.vue, src/components/TheWelcome.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/navbar.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+
+  <Navbar/>
+
+
+  <main class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <TheWelcome />
+  </main>
+</template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,97 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <nav class="border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="https://cdn.lunaiz.com/lunaiz_logo/logo-l.svg" class="h-9" alt="lunaiz logo" />
+      </a>
+      <button data-collapse-toggle="navbar-dropdown" type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-dropdown" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+        <ul
+          class="flex flex-col font-medium text-lg p-4 md:p-0 mt-4 border border-gray-100 rounded md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <button id="navlink-1" data-dropdown-toggle="navdrop-1"
+              class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">소개
+            </button>
+            <!-- Dropdown menu -->
+            <div id="navdrop-1"
+              class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                <li>
+                  <a href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">CI</a>
+                </li>
+                <li>
+                  <a href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">임원</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <button id="navlink-2" data-dropdown-toggle="navdrop-2"
+              class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">서비스
+            </button>
+            <!-- Dropdown menu -->
+            <div id="navdrop-2"
+              class="z-10 hidden font-normal  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                <li>
+                  <a href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">AM to MXM</a>
+                </li>
+                <li>
+                  <a href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">보겸 당첨 알리미</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <button id="navlink-3" data-dropdown-toggle="navdrop-3"
+              class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">미디어
+            </button>
+            <!-- Dropdown menu -->
+            <div id="navdrop-3"
+              class="z-10 hidden font-normal  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                <li>
+                  <a href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">공지사항</a>
+                </li>
+                <li>
+                  <a href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">보도자료</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">파트너</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
