a423055a1d877628214da4e687761145ca24c782
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-23 14:28:18 +0900

    Refactor hero component to add dot image in heading

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -2,10 +2,10 @@
     <div class="max-w-screen-xl flex flex-col flex-wrap items-center justify-between mx-auto py-[12.5rem]">
         <div class="mx-auto flex flex-wrap">
             <h1
-            class="flex mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
-                <slot name="head-txt"></slot><img class="flex img h-3 align-text-bottom" src="https://cdn.lunaiz.com/lunaiz_assets/dot.png" alt="..">
+            class="block mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
+                <slot name="head-txt"></slot>
             </h1>
-            <img class="flex img h-3 align-text-bottom" src="https://cdn.lunaiz.com/lunaiz_assets/dot.png" alt="..">
+            <img class="block img h-3 mt-auto mb-5 pl-1" src="https://cdn.lunaiz.com/lunaiz_assets/dot.png" alt="..">
         </div>
     </div>
 </template>
